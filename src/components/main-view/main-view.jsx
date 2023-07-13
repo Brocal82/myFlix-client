b722a4bf679e6212ff6b13789b7b134a23b1570c
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-13 08:51:22 +0200

    Replacing movies array
    If applied this commit will delete the array and will try to fetch the info from my-flix-app1982

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,46 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: 'Inception',
-      Genre: 'Science Fiction',
-      Description: 'A mind-bending heist thrillers set within the architecture of dreams.',
-      Director: 'Christopher Nolan',
-      ImageUrl: 'inception.jpg',
-    },
-    {
-      id: 2,
-      Title: 'The Dark Knight',
-      Genre: 'Action',
-      Description: 'A gritty crime thriller featuring the iconic superhero Batman.',
-      Director: 'Christopher Nolan',
-      ImageUrl: 'darkknight.jpg',
-    },
-    {
-      id:3,
-      Title: 'Interstellar',
-      Genre: 'Science Fiction',
-      Description: 'An epic space exploration journey to save humanity from a dying Earth.',
-      Director: 'Christopher Nolan',
-      ImageUrl: 'interstellar.jpg',
-    },
-    {
-      id:4,
-      Title: 'Top Gun',
-      Genre: 'Action',
-      Description: 'A mind-bending heist thrillers set within the architecture of dreams.',
-      Director: 'Tony Scott',
-      ImageUrl: 'topgun.jpg',
-    }
-
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+
+
+
+
+
+  useEffect(() => {
+    fetch("https://my-flix-app1982-c9c41fd3e5b8.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        
+        console.log(data); //provisional to find the error
+    
+        const moviesFromApi = data.movies.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            genre: doc.Genre.Name,
+            genreDescription: doc.Genre.Description,
+            director: {
+              name: doc.Director.Name,
+              bio: doc.Director.Bio,
+              birthYear: doc.Director.BirthYear,
+              deathYear: doc.Director.DeathYear,
+            },
+            actors: doc.Actors,
+            imageUrl: doc.ImageUrl,
+            featured: doc.Featured,
+          };
+        });
+        setMovies(moviesFromApi);
+      })
+      
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
+  }, []);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -65,3 +78,4 @@ export const MainView = () => {
     </div>
   );
 };
+
