c9c75cd92256b717bfd981b1729cf2a51cbcdb62
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-14 17:54:50 +0200

    fixing error key prop
    If applied this commit will change the key prop .id for ._id

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27224,7 +27224,7 @@ const MainView = ()=>{
                 onMovieClick: (newSelectedMovie)=>{
                     setSelectedMovie(newSelectedMovie);
                 }
-            }, movie.id, false, {
+            }, movie._id, false, {
                 fileName: "src/components/main-view/main-view.jsx",
                 lineNumber: 58,
                 columnNumber: 9
