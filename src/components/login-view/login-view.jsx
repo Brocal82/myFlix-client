3a0d975998eaa513c4c52a9828183beb00362d92
Brocal82 <antonioruizbrocal@gmail.com>
2023-07-25 21:12:44 +0200

    adding Form
    If applied this commit will add the Form, Form.Group and Form.control to the to the sign in form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -36,27 +39,35 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
+    <Form onSubmit={handleSubmit}>
+      <h3 className="white-text">Log in:</h3>
+      <Form.Group controlId="formUsername">
+        <Form.Label className="white-text">Username:</Form.Label>
+        <Form.Control 
+          className="bg-input"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
         />
-      </label>
+      </Form.Group>
 
-      <label>
-        Password:
-        <input
+      <Form.Group controlId="formPassword">
+        <Form.Label className="white-text">Password:</Form.Label>
+        <Form.Control
+          className="bg-input"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+          />
+      </Form.Group>
+
+      <Button className="submit-button" variant="primary" type="submit">Submit</Button>
+    </Form>
   );
 };
+
+      
+
+      
